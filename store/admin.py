db9b95324242b04788e598211f5999cbbdcbdbaf
StanMathers <34790002+StanMathers@users.noreply.github.com>
2023-01-09 23:29:27 +0400

    Added images

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Categories, Products, ProductsCategory
+from .models import Categories, Products, ProductsCategory, Images
 
 # Register your models here.
 admin.site.register(Categories)
 admin.site.register(Products)
 admin.site.register(ProductsCategory)
+admin.site.register(Images)
 
--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from ecommerce_api.settings import AUTH_USER_MODEL
 # Create your models here.
 class Categories(models.Model):
     name = models.CharField(max_length=255)
-    
+
     def __str__(self):
         return self.name
 
@@ -15,16 +15,19 @@ class Products(models.Model):
     discount = models.PositiveBigIntegerField(blank=True, null=True)
     stock = models.PositiveBigIntegerField(blank=True, null=True)
     owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.SET_DEFAULT, default=1)
-    
+
     def __str__(self):
         return self.name
-    
 
 class ProductsCategory(models.Model):
     # Assosiation table
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f'{self.product.name} - {self.category.name}'
 
+    def __str__(self):
+        return f"{self.product.name} - {self.category.name}"
+
+
+class Images(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="media_root/")
