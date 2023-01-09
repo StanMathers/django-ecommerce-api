e93a55d5e210f2aa15ef62db488959b93ae79086
StanMathers <34790002+StanMathers@users.noreply.github.com>
2023-01-09 23:09:24 +0400

    Added Owner field to Products

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categories, Products, ProductsCategory
 
 # Register your models here.
+admin.site.register(Categories)
+admin.site.register(Products)
+admin.site.register(ProductsCategory)
+
--- store/models.py
+++ store/models.py
@@ -1,19 +1,30 @@
 from django.db import models
+from ecommerce_api.settings import AUTH_USER_MODEL
 
 # Create your models here.
 class Categories(models.Model):
     name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
 
 class Products(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     unit_price = models.PositiveBigIntegerField()
     discount = models.PositiveBigIntegerField(blank=True, null=True)
     stock = models.PositiveBigIntegerField(blank=True, null=True)
+    owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.SET_DEFAULT, default=1)
+    
+    def __str__(self):
+        return self.name
     
 
 class ProductsCategory(models.Model):
     # Assosiation table
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.product.name} - {self.category.name}'
 
